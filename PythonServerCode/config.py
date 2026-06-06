709d7d8c02e2ba537675abbbba14532a7b7f2fc2
JoFil24 <filipjoveski2407@gmail.com>
2026-06-06 15:50:20 +0200

    Changed allowed IP address

--- PythonServerCode/config.py
+++ PythonServerCode/config.py
@@ -1,5 +1,5 @@
 API_KEY = "changeme-use-a-long-random-string"
-ALLOWED_IPS = {"192.168.56.1", "127.0.0.1", "::1"}
+ALLOWED_IPS = {"192.168.56.20", "127.0.0.1", "::1"}
 RESULTS_DIR = "./results"
 
 # ZAP settings
